5e2ebe17811a2271257fdd50fba42cc59da79659
liuyuanbo <1131107655@qq.com>
2023-07-18 18:28:28 +0800

    组件通信-props

--- 代码/vue3-communication/src/views/01_props/Child.vue
+++ 代码/vue3-communication/src/views/01_props/Child.vue
@@ -2,10 +2,14 @@
   <div class="box">
     <h3>Child</h3>
     <div @click="changeMsg">msg:{{ msg }}</div>
+    <div @click="changeName">userInfo.name:{{ userInfo.name }}</div>
+    <button @click="changeHandler">修改userInfo</button>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { reactive } from 'vue'
+
 const props = defineProps<{
   msg:string,
   userInfo:{
@@ -16,6 +20,19 @@ const props = defineProps<{
 }>()
 
 const changeMsg = () => {
-  props.msg = 'aa'//不能直接修改父元素中的数据
+  // props.msg = 'aa'//不能直接修改父元素中的数据
+}
+
+const changeName = () => {
+  props.userInfo.name = '孙悟空'//只能改属性
+
+  // props.userInfo = reactive({// 不能改地址
+  //   name:'弼马温',
+  //   age:11
+  // })
+}
+
+const changeHandler = () => {
+  props.changeUserInfo()//父组件传递过来的方法
 }
 </script>
