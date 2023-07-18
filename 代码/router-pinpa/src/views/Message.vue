dac0f1b5e9a1d9428f97a36af5ad448c9271fddb
liuyuanbo <1131107655@qq.com>
2023-07-18 14:41:28 +0800

    三级路由组件拆分-传参

--- 代码/router-pinpa/src/views/Message.vue
+++ 代码/router-pinpa/src/views/Message.vue
@@ -1,29 +1,49 @@
 <template>
     <div role="tabpanel" class="tab-pane active" id="Message">
         <ul class="list-group">
-            <li class="list-group-item">
-                <a href="##">message01</a>
+            <li class="list-group-item"
+            v-for="item in list"
+            :key="item.id"
+            >
+            <!-- <router-link to="/home/message/msgdetail">{{ item.content }}</router-link> -->
+            <!-- /home/message/msgdetail/10010?content=王宝强 -->
+
+            <!-- <router-link :to="`/home/message/msgdetail/${ item.id }?content=${ item.content }`">{{ item.content }}</router-link> -->
+
+            <router-link :to="{
+                // path:'/home/message/msgdetail',
+                name:'MsgDetail',
+                params:{
+                    msgId:item.id
+                },
+                query:{
+                    content:item.content
+                }
+            }">{{ item.content }}</router-link>
+
+                <!-- <a href="##">message01</a>
             </li>
             <li class="list-group-item">
                 <a href="##">message02</a>
             </li>
             <li class="list-group-item">
-                <a href="##">message03</a>
+                <a href="##">message03</a> -->
             </li>
         </ul>
         <div class="alert alert-success" role="alert">
-            <ul class="list-group">
-                <li class="list-group-item">message01</li>
-                <li class="list-group-item">message02</li>
-                <li class="list-group-item">message03</li>
-                <li class="list-group-item">message04</li>
-            </ul>
+            <router-view></router-view>
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
 
+const list = ref([
+    {id:1,content:'悟空'},
+    {id:2,content:'八戒'},
+    {id:3,content:'唐僧'}
+])
 </script>
 
 <style scoped></style>
---
+++ 代码/router-pinpa/src/views/MsgDetail.vue
@@ -0,0 +1,20 @@
+<template>
+    <ul class="list-group">
+      <li class="list-group-item">消息id: {{ route.params.msgId }}</li>
+      <li class="list-group-item">消息content: {{ route.query.content }}</li>
+      <hr>
+      <li class="list-group-item">消息id: {{ msgId }}</li>
+      <li class="list-group-item">消息content: {{ content }}</li>
+      <li class="list-group-item">text: {{ text }}</li>
+    </ul>
+  </template>
+  
+  <script setup lang="ts">
+  import { useRoute } from 'vue-router'
+  const route = useRoute() // 调用useRoute得到当前路由
+  
+  defineProps(['msgId', 'content', 'text'])
+  
+  </script>
+  
+  <style scoped></style>
