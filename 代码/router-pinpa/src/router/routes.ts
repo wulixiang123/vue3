31f9441787a89ffab83832d1e42d502d2c6dbf34
liuyuanbo <1131107655@qq.com>
2023-07-18 14:12:17 +0800

    二级路由组件拆分

--- 代码/router-pinpa/src/router/routes.ts
+++ 代码/router-pinpa/src/router/routes.ts
@@ -3,7 +3,21 @@ import type { RouteRecordRaw } from 'vue-router'
 const routes:RouteRecordRaw[] = [
     {
         path:'/home',
-        component:()=>import('@/views/Home.vue')
+        component:()=>import('@/views/Home.vue'),
+        children:[
+            {
+                path:'message',
+                component:()=>import('@/views/Message.vue')
+            },
+            {
+                path:'news',
+                component:()=>import('@/views/News.vue')
+            },
+            {
+                path:'',
+                redirect:'/home/message'
+            }
+        ]
     },
     {
         path:'/about',
---
+++ 代码/router-pinpa/src/views/News.vue
@@ -0,0 +1,33 @@
+<template>
+    <div role="tabpanel" class="tab-panel" id="News">
+        <ul class="list-group">
+            <li class="list-group-item">
+                news1
+                <button type="button" class="btn btn-primary" data-toggle="button" aria-pressed="false" autocomplete="off">
+                </button>
+            </li>
+            <li class="list-group-item">
+                news2
+                <button type="button" class="btn btn-primary" data-toggle="button" aria-pressed="false" autocomplete="off">
+                </button>
+            </li>
+            <li class="list-group-item">
+                news3
+                <button type="button" class="btn btn-primary" data-toggle="button" aria-pressed="false" autocomplete="off">
+                </button>
+            </li>
+        </ul>
+        <div class="alert alert-success" role="alert">
+            <ul class="list-group">
+                <li class="list-group-item">news1</li>
+                <li class="list-group-item">news2</li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped></style>
